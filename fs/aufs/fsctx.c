4fecc7015a5b301797228f31668cf2f62b23b8da
J. R. Okajima <hooanon05g@gmail.com>
2015-03-30 07:49:31 +0900

    aufs: dirwh option
    This is a feature to optimize for rmdir and rename dir.
    When the number of whiteouts under the target dir is very many, it may
    take a long time to remove them all. To prevent this, 'dirwh=%d' option
    specifies the watermark to decide when to remove them.
    
    For details, see aufs manual in aufs-util.git.
    
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- fs/aufs/fsctx.c
+++ fs/aufs/fsctx.c
@@ -218,6 +218,9 @@ static void au_fsctx_dump(struct au_opts *opts)
 				  u.add->path.dentry);
 			break;
 
+		case Opt_dirwh:
+			AuDbg("dirwh %d\n", opt->dirwh);
+			break;
 		case Opt_rdcache:
 			AuDbg("rdcache %d\n", opt->rdcache);
 			break;
@@ -332,6 +335,8 @@ const struct fs_parameter_spec aufs_fsctx_paramspec[] = {
 	/* fsparam_s32("idel", Opt_idel), */
 	fsparam_path("mod", Opt_mod),
 	/* fsparam_string("imod", Opt_imod), */
+
+	fsparam_s32("dirwh", Opt_dirwh),
 
 	fsparam_path("xino", Opt_xino),
 	fsparam_flag("noxino", Opt_noxino),
@@ -811,6 +816,11 @@ static int au_fsctx_parse_param(struct fs_context *fc, struct fs_parameter *para
 		}
 		err = au_fsctx_parse_xino_itrunc(fc, &opt->xino_itrunc,
 						 result.int_32);
+		break;
+
+	case Opt_dirwh:
+		err = 0;
+		opt->dirwh = result.int_32;
 		break;
 
 	case Opt_rdcache:
