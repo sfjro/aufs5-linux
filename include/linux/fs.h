55a06eecdd484712c8ed954ab4a06aea54495a77
J. R. Okajima <hooanon05g@gmail.com>
2015-03-30 07:49:16 +0900

    for aufs mmap: print the virtual file path
    For details, read the document in later commit.
    I don't like this approach, and UnionMount seems to try solving a
    similar problem by customizing f_dentry and d_inode. Ideally it is
    better to result the same fix, but it may not be so easy because
    UnionMount doesn't have its virtual inode/dentry/file objects as it is
    not a 'filesystem.'
    
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2463,6 +2463,21 @@ struct file *dentry_create(const struct path *path, int flags, umode_t mode,
 			   const struct cred *cred);
 const struct path *backing_file_user_path(const struct file *f);
 
+#if IS_MODULE(CONFIG_AUFS_FS)
+/* fs/au_mf.c */
+const struct path *au_file_user_path(const struct file *f);
+const struct inode *au_file_user_inode(const struct file *f);
+#elif IS_BUILTIN(CONFIG_AUFS_FS)
+/* fs/aufs/mf.c */
+const struct path *au_do_file_user_path(const struct file *f);
+const struct inode *au_do_file_user_inode(const struct file *f);
+#define au_file_user_path(f)	au_do_file_user_path(f)
+#define au_file_user_inode(f)	au_do_file_user_inode(f)
+#else
+#define au_file_user_path(f)	NULL
+#define au_file_user_inode(f)	NULL
+#endif
+
 /*
  * When mmapping a file on a stackable filesystem (e.g., overlayfs), the file
  * stored in ->vm_file is a backing file whose f_inode is on the underlying
@@ -2475,15 +2490,25 @@ const struct path *backing_file_user_path(const struct file *f);
 /* Get the path to display in /proc/<pid>/maps */
 static inline const struct path *file_user_path(const struct file *f)
 {
+	const struct path *path;
+
 	if (unlikely(f->f_mode & FMODE_BACKING))
 		return backing_file_user_path(f);
+	path = au_file_user_path(f);
+	if (path)
+		return path;
 	return &f->f_path;
 }
 /* Get the inode whose inode number to display in /proc/<pid>/maps */
 static inline const struct inode *file_user_inode(const struct file *f)
 {
+	const struct inode *inode;
+
 	if (unlikely(f->f_mode & FMODE_BACKING))
 		return d_inode(backing_file_user_path(f)->dentry);
+	inode = au_file_user_inode(f);
+	if (inode)
+		return inode;
 	return file_inode(f);
 }
 
