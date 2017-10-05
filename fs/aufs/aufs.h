b48c328a79bc7e16a4265f1149b07ca60084fdd6
J. R. Okajima <hooanon05g@gmail.com>
2017-10-05 21:33:47 +0900

    aufs: kmalloc/kfree wrappers
    Very basic, simple and stupid wrappers for kmalloc family.
    It tries kfree_ruc() as possible, with hoping a better performance.
    
    Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

---
+++ fs/aufs/aufs.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2005-2026 Junjiro R. Okajima
+ */
+
+/*
+ * all header files
+ */
+
+#ifndef __AUFS_H__
+#define __AUFS_H__
+
+#ifdef __KERNEL__
+
+#define AuStub(type, name, body, ...) \
+	static inline type name(__VA_ARGS__) { body; }
+
+#define AuStubVoid(name, ...) \
+	AuStub(void, name, , __VA_ARGS__)
+#define AuStubInt0(name, ...) \
+	AuStub(int, name, return 0, __VA_ARGS__)
+
+#include "module.h"
+/* add more later */
+
+#endif /* __KERNEL__ */
+#endif /* __AUFS_H__ */
