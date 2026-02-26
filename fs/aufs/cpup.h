sfjro/aufs4-linux

aufs: copy-up 1/7, attributes
Copy the inode attributes between branches.
See also the document in this commit.

Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

fs/aufs/cpup.h
@@ -24,9 +24,28 @@
 
 #ifdef __KERNEL__
 
+#include <linux/path.h>
+
 struct inode;
 
+void au_cpup_attr_flags(struct inode *dst, unsigned int iflags);
+void au_cpup_attr_timesizes(struct inode *inode);
+void au_cpup_attr_nlink(struct inode *inode, int force);
+void au_cpup_attr_changeable(struct inode *inode);
 void au_cpup_igen(struct inode *inode, struct inode *h_inode);
+void au_cpup_attr_all(struct inode *inode, int force);
+
+/* ---------------------------------------------------------------------- */
+
+/* keep timestamps when copyup */
+struct au_dtime {
+	struct dentry *dt_dentry;
+	struct path dt_h_path;
+	struct timespec dt_atime, dt_mtime;
+};
+void au_dtime_store(struct au_dtime *dt, struct dentry *dentry,
+		    struct path *h_path);
+void au_dtime_revert(struct au_dtime *dt);
 
 #endif /* __KERNEL__ */
 #endif /* __AUFS_CPUP_H__ */

sfjro/aufs5-linux

fs/aufs/cpup.h
@@ -12,9 +12,29 @@
 
 #ifdef __KERNEL__
 
+#include <linux/path.h>
+#include <linux/time.h>
+
 struct inode;
 
+void au_cpup_attr_flags(struct inode *dst, unsigned int iflags);
+void au_cpup_attr_timesizes(struct inode *inode);
+void au_cpup_attr_nlink(struct inode *inode, int force);
+void au_cpup_attr_changeable(struct inode *inode);
 void au_cpup_igen(struct inode *inode, struct inode *h_inode);
+void au_cpup_attr_all(struct inode *inode, int force);
+
+/* ---------------------------------------------------------------------- */
+
+/* keep timestamps when copyup */
+struct au_dtime {
+	struct dentry *dt_dentry;
+	struct path dt_h_path;
+	struct timespec64 dt_atime, dt_mtime;
+};
+void au_dtime_store(struct au_dtime *dt, struct dentry *dentry,
+		    struct path *h_path);
+void au_dtime_revert(struct au_dtime *dt);
 
 #endif /* __KERNEL__ */
 #endif /* __AUFS_CPUP_H__ */