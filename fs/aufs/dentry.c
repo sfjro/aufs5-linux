sfjro/aufs-standalone

aufs: dentry op, opt-out ->d_revalidate()
Optimize out ->d_revalidate() if possible.
If the refreshing failed, then ->d_revalidate() remains. In this case,
aufs has two types of dentries. One has ->d_revalidate, the other
doesn't. I am afraid it will confuse me someday.

Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

fs/aufs/dentry.c
@@ -655,6 +655,28 @@ out:
 	return err;
 }
 
+void au_refresh_dop(struct dentry *dentry, int force_reval)
+{
+	const struct dentry_operations *dop
+		= force_reval ? &aufs_dop : dentry->d_sb->s_d_op;
+	static const unsigned int mask
+		= DCACHE_OP_REVALIDATE | DCACHE_OP_WEAK_REVALIDATE;
+
+	BUILD_BUG_ON(sizeof(mask) != sizeof(dentry->d_flags));
+
+	if (dentry->d_op == dop)
+		return;
+
+	AuDbg("%pd\n", dentry);
+	spin_lock(&dentry->d_lock);
+	if (dop == &aufs_dop)
+		dentry->d_flags |= mask;
+	else
+		dentry->d_flags &= ~mask;
+	dentry->d_op = dop;
+	spin_unlock(&dentry->d_lock);
+}
+
 int au_refresh_dentry(struct dentry *dentry, struct dentry *parent)
 {
 	int err, ebrange, nbr;
@@ -1062,5 +1084,10 @@ static void aufs_d_release(struct dentry *dentry)
 const struct dentry_operations aufs_dop = {
 	.d_revalidate		= aufs_d_revalidate,
 	.d_weak_revalidate	= aufs_d_revalidate,
+	.d_release		= aufs_d_release
+};
+
+/* aufs_dop without d_revalidate */
+const struct dentry_operations aufs_dop_noreval = {
 	.d_release		= aufs_d_release
 };

sfjro/aufs5-linux

fs/aufs/dentry.c
@@ -670,6 +670,28 @@ out:
 	return err;
 }
 
+void au_refresh_dop(struct dentry *dentry, int force_reval)
+{
+	const struct dentry_operations *dop
+		= force_reval ? &aufs_dop : dentry->d_sb->__s_d_op;
+	static const unsigned int mask
+		= DCACHE_OP_REVALIDATE | DCACHE_OP_WEAK_REVALIDATE;
+
+	BUILD_BUG_ON(sizeof(mask) != sizeof(dentry->d_flags));
+
+	if (dentry->d_op == dop)
+		return;
+
+	AuDbg("%pd\n", dentry);
+	spin_lock(&dentry->d_lock);
+	if (dop == &aufs_dop)
+		dentry->d_flags |= mask;
+	else
+		dentry->d_flags &= ~mask;
+	dentry->d_op = dop;
+	spin_unlock(&dentry->d_lock);
+}
+
 int au_refresh_dentry(struct dentry *dentry, struct dentry *parent)
 {
 	int err, ebrange, nbr;
@@ -1138,5 +1160,10 @@ static void aufs_d_release(struct dentry *dentry)
 const struct dentry_operations aufs_dop = {
 	.d_revalidate		= aufs_d_revalidate,
 	.d_weak_revalidate	= aufs_d_weak_revalidate,
+	.d_release		= aufs_d_release
+};
+
+/* aufs_dop without d_revalidate */
+const struct dentry_operations aufs_dop_noreval = {
 	.d_release		= aufs_d_release
 };