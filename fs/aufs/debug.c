sfjro/aufs4-linux

aufs: white-out 2/2, diropq
The marker to represent that the directory is opaque (stop digging
down the branch stack) is implemented as a special whiteout.

See also the document in previous commit.

Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

fs/aufs/debug.c
@@ -152,9 +152,9 @@ void au_dpri_dentry(struct dentry *dentry)
 	dinfo = au_di(dentry);
 	if (!dinfo)
 		return;
-	dpri("d-1: bstart %d, bend %d, bwh %d, gen %d\n",
+	dpri("d-1: bstart %d, bend %d, bwh %d, bdiropq %d, gen %d\n",
 	     dinfo->di_bstart, dinfo->di_bend,
-	     dinfo->di_bwh, au_digen(dentry));
+	     dinfo->di_bwh, dinfo->di_bdiropq, au_digen(dentry));
 	if (dinfo->di_bstart < 0)
 		return;
 	hdp = dinfo->di_hdentry;

sfjro/aufs5-linux

fs/aufs/debug.c
@@ -141,9 +141,9 @@ void au_dpri_dentry(struct dentry *dentry)
 	dinfo = au_di(dentry);
 	if (!dinfo)
 		return;
-	dpri("d-1: btop %d, bbot %d, bwh %d, gen %d\n",
+	dpri("d-1: btop %d, bbot %d, bwh %d, bdiropq %d, gen %d\n",
 	     dinfo->di_btop, dinfo->di_bbot,
-	     dinfo->di_bwh, au_digen(dentry));
+	     dinfo->di_bwh, dinfo->di_bdiropq, au_digen(dentry));
 	if (dinfo->di_btop < 0)
 		return;
 	for (bindex = dinfo->di_btop; bindex <= dinfo->di_bbot; bindex++)