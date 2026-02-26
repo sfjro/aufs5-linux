sfjro/aufs4-linux

aufs: white-out 1/2
The whiteout represents a logical deletion.
Although the document in this commit mentioned about rmdir(2) and
rename(2) for dir, this commit doesn't contain such functions. They will
be added in later commits.

Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

fs/aufs/debug.c
@@ -152,9 +152,9 @@ void au_dpri_dentry(struct dentry *dentry)
 	dinfo = au_di(dentry);
 	if (!dinfo)
 		return;
-	dpri("d-1: bstart %d, bend %d, gen %d\n",
+	dpri("d-1: bstart %d, bend %d, bwh %d, gen %d\n",
 	     dinfo->di_bstart, dinfo->di_bend,
-	     au_digen(dentry));
+	     dinfo->di_bwh, au_digen(dentry));
 	if (dinfo->di_bstart < 0)
 		return;
 	hdp = dinfo->di_hdentry;

sfjro/aufs5-linux

fs/aufs/debug.c
@@ -141,9 +141,9 @@ void au_dpri_dentry(struct dentry *dentry)
 	dinfo = au_di(dentry);
 	if (!dinfo)
 		return;
-	dpri("d-1: btop %d, bbot %d, gen %d\n",
+	dpri("d-1: btop %d, bbot %d, bwh %d, gen %d\n",
 	     dinfo->di_btop, dinfo->di_bbot,
-	     au_digen(dentry));
+	     dinfo->di_bwh, au_digen(dentry));
 	if (dinfo->di_btop < 0)
 		return;
 	for (bindex = dinfo->di_btop; bindex <= dinfo->di_bbot; bindex++)