sfjro/aufs4-linux

aufs: sbinfo core
The structure is very similar to inode and dentry infos (in previous
commits), but the internal array is for 'struct au_branch' instead of
'superblock.' Additionally the lifetime of 'struct au_sbinfo' is managed
by kobject since it will be connected to sysfs by later commit.

Signed-off-by: J. R. Okajima <hooanon05g@gmail.com>

fs/aufs/super.c
@@ -75,7 +75,23 @@ struct inode *au_iget_locked(struct super_block *sb, ino_t ino)
 
 out:
 	/* never return NULL */
+	AuDebugOn(!inode);
+	AuTraceErrPtr(inode);
 	return inode;
+}
+
+/* ---------------------------------------------------------------------- */
+
+/* final actions when unmounting a file system */
+static void aufs_put_super(struct super_block *sb)
+{
+	struct au_sbinfo *sbinfo;
+
+	sbinfo = au_sbi(sb);
+	if (!sbinfo)
+		return;
+
+	kobject_put(&sbinfo->si_kobj);
 }
 
 /* ---------------------------------------------------------------------- */
@@ -84,5 +100,6 @@ static const struct super_operations aufs_sop = {
 	.alloc_inode	= aufs_alloc_inode,
 	.destroy_inode	= aufs_destroy_inode,
 	/* always deleting, no clearing */
-	.drop_inode	= generic_delete_inode
+	.drop_inode	= generic_delete_inode,
+	.put_super	= aufs_put_super
 };

sfjro/aufs5-linux

fs/aufs/super.c
@@ -68,10 +68,23 @@ out:
 
 /* ---------------------------------------------------------------------- */
 
+/* final actions when unmounting a file system */
+static void aufs_put_super(struct super_block *sb)
+{
+	struct au_sbinfo *sbinfo;
+
+	sbinfo = au_sbi(sb);
+	if (sbinfo)
+		kobject_put(&sbinfo->si_kobj);
+}
+
+/* ---------------------------------------------------------------------- */
+
 const struct super_operations aufs_sop = {
 	.alloc_inode	= aufs_alloc_inode,
 	.destroy_inode	= aufs_destroy_inode,
 	.free_inode	= aufs_free_inode,
 	/* always deleting, no clearing */
-	.drop_inode	= inode_just_drop
+	.drop_inode	= inode_just_drop,
+	.put_super	= aufs_put_super
 };